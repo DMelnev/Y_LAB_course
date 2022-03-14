6076b42eaba6f966ec211523a7fcfc57daa696a0
DMITRIY <dvmau@mail.ru>
2022-03-14 11:30:08 +0300

    did cp-1251 code

--- Lessons/src/Lesson_3/Test.java
+++ Lessons/src/Lesson_3/Test.java
@@ -9,10 +9,11 @@ package Lesson_3;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         try {
             Data root2 = ParserXML.dataFromFile("victim.xml");
@@ -24,8 +25,10 @@ public class Test {
                 file2.createNewFile();
             }
 
-            try (FileOutputStream outputStream = new FileOutputStream(file2)) {
-                outputStream.write(ParserXML.dataToXML(root2).toString().getBytes(StandardCharsets.UTF_8));
+
+            try (FileOutputStream outputStream = new FileOutputStream(file2);//){
+                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "windows-1251")) {
+                outputStreamWriter.write(ParserXML.dataToXML(root2).toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
