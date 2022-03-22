b61b424eafa7c5ebaf1fb55be8f2d9d4f2b1b1d6
DMITRIY <dvmau@mail.ru>
2022-03-22 03:13:16 +0300

    made dataToString

--- Lessons/src/Lesson_4/Main.java
+++ Lessons/src/Lesson_4/Main.java
@@ -5,6 +5,7 @@ import Lesson_4.Data.Encoding;
 import Lesson_4.Game.Game;
 import Lesson_4.Parser.FileWorker;
 import Lesson_4.Parser.MyParser;
+import Lesson_4.Parser.ParserJSON;
 import Lesson_4.Parser.ParserXML;
 import Lesson_4.Player.Player;
 
@@ -22,19 +23,28 @@ public class Main {
 
         Data root = new Data("GamePlay");
 
-        Game game = new Game(root);
-        game.run();
-
+//        Game game = new Game(root);
+//        game.run();
+//
         MyParser parser = new ParserXML();
+        MyParser parser2 = new ParserJSON();
+
         parser.setCharSet(Encoding.WINDOWS1251);
+//
+//        String string = parser.dataToString(root);
+//
+//        FileWorker.setCharSet(Encoding.WINDOWS1251);
+//        if (!FileWorker.writeFile(string, FILE_NAME)) System.out.println("Не удалось записать в файл!");
+//
+//        Player player = new Player();
+//        player.FromFile("log.xml");
 
-        String string = parser.dataToString(root);
+        root = parser.stringToData(FileWorker.readFile(FILE_NAME).toString());
 
-        FileWorker.setCharSet(Encoding.WINDOWS1251);
-        if (!FileWorker.writeFile(string, FILE_NAME)) System.out.println("Не удалось записать в файл!");
+        String test = parser2.dataToString(root);
 
-        Player player = new Player();
-        player.FromFile("log.xml");
+        FileWorker.writeFile(test,"test.JSON");
+        System.out.println(test);
 
     }
 }
--- Lessons/src/Lesson_4/Parser/ParserJSON.java
+++ Lessons/src/Lesson_4/Parser/ParserJSON.java
@@ -8,26 +8,25 @@ package Lesson_4.Parser;
 
 import Lesson_4.Data.Data;
 import Lesson_4.Data.Encoding;
-import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 
-import java.util.List;
-import java.util.Map;
-
+import java.util.HashMap;
 
 public class ParserJSON implements MyParser {
 
     private Encoding charSet = Encoding.UTF8;
+    private static final StringBuilder INDENT = new StringBuilder("    "); //отступ
 
 
     @Override
     public Data stringToData(String string) {
-        Data data = new Data("GamePlay");
-        if (string.equals("")) return null;
+//        Data data = new Data("GamePlay");
+//        if (string.equals("")) return null;
 
-        List<Map<String, Object>> res = (List<Map<String, Object>>) JsonPath.parse(string);
+//        List<Map<String, Object>> res = (List<Map<String, Object>>) JsonPath.parse(string);
 
-        res.forEach(System.out::println);
-
+//        res.forEach(System.out::println);
 
 
         return null;
@@ -35,11 +34,88 @@ public class ParserJSON implements MyParser {
 
     @Override
     public String dataToString(Data root) {
-        return null;
+        JSONObject result = new JSONObject();
+        JSONArray main = new JSONArray();
+
+        for (Data mainNode : root.getChildNodes()) {
+            if (mainNode.getTagName().equals("Player")) {
+                JSONObject player = new JSONObject();
+                player.put("Player", getAttributes(mainNode.getAttributes()));
+                main.add(player);
+            }
+            if (mainNode.getTagName().equals("Game")) {
+                JSONObject game = new JSONObject();
+                JSONArray list = new JSONArray();
+                JSONObject attributes = new JSONObject();
+                JSONObject steps = new JSONObject();
+                JSONArray stp = new JSONArray();
+
+                attributes.put("Attr", getAttributes(mainNode.getAttributes()));
+
+                for (Data step : mainNode.getChildNodes()) {
+                    JSONObject stepJ = new JSONObject();
+                    JSONObject tmp = getAttributes(step.getAttributes());
+                    if (step.getTagName().equals("GameResult")) {
+                        if (step.getText().equals("Draw!")) {
+                            stepJ.put("GameResult", "Draw!");
+                        } else {
+                            JSONObject playerWin = getAttributes(step.getChildNodeByName("Player").getAttributes());
+                            JSONObject playerWinM = new JSONObject();
+                            playerWinM.put("Player", playerWin);
+                            stepJ.put("GameResult", playerWinM);
+                        }
+                    } else {
+                        tmp.put("Coordinate", step.getText());
+                        stepJ.put("Step", tmp);
+                    }
+                    stp.add(stepJ);
+                }
+
+                steps.put("Steps", stp);
+                list.add(attributes);
+                list.add(steps);
+                game.put("Game", list);
+                main.add(game);
+            }
+        }
+
+        result.put(root.getTagName(), main);
+        return formatString(result.toJSONString());
     }
 
     @Override
     public void setCharSet(Encoding charSet) {
         this.charSet = charSet;
+    }
+
+    private JSONObject getAttributes(HashMap<String, String> attr) {
+        JSONObject res = new JSONObject();
+        if (attr.size() > 0) {
+            attr.forEach((k, v) -> res.put(k, v));
+        }
+        return res;
+    }
+
+    private String formatString(String string) {
+        StringBuilder temp = new StringBuilder(string);
+        StringBuilder tempIndent = new StringBuilder();
+
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp.charAt(i) == '{') {
+                temp.insert(i + 1, "\n" + tempIndent);
+                if (i != 0) temp.insert(i, "\n" + tempIndent);
+                i += tempIndent.length() + 1;
+                tempIndent = tempIndent.append(INDENT);
+            }
+            if (temp.charAt(i) == '}' && i < (temp.length() - 1)) {
+                tempIndent = new StringBuilder(tempIndent.substring(INDENT.length()));
+                if (i > 0) temp.insert(i, "\n" + tempIndent);
+                i += tempIndent.length() + ((temp.charAt(i + 1) == ',') ? 2 : 1);
+            }
+            if (i > 0 && temp.charAt(i) == ',' && (temp.charAt(i - 1) == '"' || temp.charAt(i - 1) == ']')) {
+                temp.insert(i + 1, "\n" + tempIndent.substring(INDENT.length()));
+            }
+        }
+        return temp.toString();
     }
 }
